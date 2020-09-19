a2864e753d48629062f337ae3b7bf08748128c3b
givemo <gtchiwanza@gmail.com>
2020-09-19 16:05:02 +0100

    fixed broken delete links

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-import {getUsers} from "./api/userApi"
+import "./index.css";
+
+import { getUsers, deleteUser } from "./api/userApi";
 
 //Populate table of users via API call.
-getUsers().then(result => {
-    let userBody = "";
+getUsers().then((result) => {
+  let userBody = "";
 
-    result.forEach(user => {
-        userBody += `<tr>
-            <td><a href="#" data-id="${user.id} class="deleteUser">Delete</a></td>
+  result.forEach((user) => {
+    userBody += `<tr>
+            <td><a href="#" data-id="${user.id}"class="deleteUser" >Delete</a></td>
             <td>${user.id}</td>
             <td>${user.firstName}</td>
             <td>${user.lastName}</td>
             <td>${user.email}</td>
-            </tr>`
-    });
+            </tr>`;
+  });
 
-    global.document.getElementById('users').innerHTML = userBody;
-})
+  global.document.getElementById("users").innerHTML = userBody;
+
+  const deleteLinks = global.document.getElementsByClassName("deleteUser");
+
+  //Must use array.from to create a real array from a DOM collection
+  //getElementByClassName only returns an "array like" object
+  Array.from(deleteLinks, (link) => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
